e173fa882f56b4b1f12bc281f207e876082e9fa2
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-09-07 14:21:11 -0300

    Manipulation we models...

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'email',
+    ordering = ('-id',)
+    # list_filter = ('created_date'), # as the name said, filter by created date
+    search_fields = ('id', 'first_name', 'last_name', 'email')
+    list_per_page = 20
+    list_max_show_all = 200
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django .utils import timezone
 
-# Create your models here.
+# First:
+# id (primary key - automatic)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+
+# After:
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (image)
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=254)
+    last_name = models.CharField(max_length=254)
+    phone = models.CharField(max_length=40)
+    email = models.EmailField(max_length=254)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+
