6f85a31b85a29d6763ff8568538f3010cf3f338f
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-09-07 12:43:49 -0300

    Arranging global files...

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request):
+    return render(
+        request=request,
+        template_name='contact/index.html',
+    )
