6c575488dc9fa4a1fc595ba0aa8b4c9584652a67
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2023-09-14 19:06:41 -0300

    Using filter to searching...

--- contact/urls.py
+++ contact/urls.py
@@ -7,5 +7,6 @@ app_name = 'contact'
 
 urlpatterns = [
     path('<int:contact_id>/', views.contact_view, name='contact_view'),
+    path('search/', views.search, name='search'),  # type: ignore
     path('', views.index_view, name='index'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-
+from django.shortcuts import get_object_or_404, redirect, render
+from django.db.models import Q
 from contact.models import Contact
 
 
@@ -8,7 +8,8 @@ def index_view(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
 
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -38,11 +39,44 @@ def contact_view(request, contact_id):
         Or like this:
     """
     contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    # flake8: noqa
     # contact = get_object_or_404(Contact.objects.filter(pk=contact_id, show=True)) We can put the manager
+    site_title = f'{contact.first_name} {contact.last_name} - '
     return render(
         request,
         'contact/contact.html',
         {
-            'contact': contact
+            'contact': contact,
+            'site_title': site_title
         }
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(
+            Q(first_name__contains = search_value)
+            | # logic operator or 
+            Q(last_name__contains = search_value) |
+            Q(phone__contains = search_value) |
+            Q(email__contains = search_value) 
+            ) \
+        .order_by('-id') \
+            
+            
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
     )
